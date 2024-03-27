5cacafd1a2bea62502d78b90f532f6de27b45c73
muhammad junaid <junaidbishi02@gmail.com>
2024-03-26 22:59:30 -0700

    first assign

---
+++ Q2-Personal Message/main.js
@@ -0,0 +1,3 @@
+"use strict";
+let personname = "AMIR";
+console.log('"AS SALAM O ALIKUM', (personname), 'Tomorrow i will coming from karachi to lahore"');
---
+++ Q2-Personal Message/main.ts
@@ -0,0 +1,4 @@
+
+let personname = "AMIR";
+
+console.log('"AS SALAM O ALIKUM',(personname), 'Tomorrow i will coming from karachi to lahore"')
